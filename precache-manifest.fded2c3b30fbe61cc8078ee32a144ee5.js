c9a3661ff4e419c52bdc8fe60abfc132a3c9ea35
Asif Iqbal <webwizo@outlook.com>
2022-04-13 16:54:31 +0500

    Updates

--- precache-manifest.fded2c3b30fbe61cc8078ee32a144ee5.js
+++ precache-manifest.fded2c3b30fbe61cc8078ee32a144ee5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc36c61be25b5fad6c38bac39afd4052",
+    "revision": "594daabe04737a68fffef84cd004a01c",
     "url": "/index.html"
   },
   {
-    "revision": "c4117aae53e9b396f40b",
-    "url": "/static/js/2.222e8274.chunk.js"
+    "revision": "ca9b58b5fff28314638f",
+    "url": "/static/js/2.f3a15d19.chunk.js"
   },
   {
     "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
-    "url": "/static/js/2.222e8274.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.f3a15d19.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bad0d342422eea7397e4",
-    "url": "/static/js/main.b02c62ee.chunk.js"
+    "revision": "ae577a5da7a4c55f734d",
+    "url": "/static/js/main.66923c24.chunk.js"
   },
   {
     "revision": "10984270f2637042c936",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c9ec7d20e6a0515e12b39e80785c1f2.js"
+  "/precache-manifest.fded2c3b30fbe61cc8078ee32a144ee5.js"
 );
 
 self.addEventListener('message', (event) => {
